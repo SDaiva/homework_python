ce15cb11ce677e119b37dc4715d375371d6627cb
SDaiva <stdaiva@gmail.com>
2023-03-02 19:39:10 +0200

    classwork push

---
+++ classwork6.py
@@ -0,0 +1,5 @@
+def my_function():
+        print("Hello world")
+
+print("======================")
+my_function()
---
+++ classwork_3_max_numbers.py
@@ -0,0 +1,7 @@
+def my_function(max_number):
+    for x in max_numbers:
+        print(x)
+
+a = (1, 55, 67)
+x = max(a)
+print(x)
