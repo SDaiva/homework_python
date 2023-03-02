bece9ad7347b0ed1921f75e3eba70d89c24c5f3a
SDaiva <stdaiva@gmail.com>
2023-03-02 20:09:05 +0200

    classwork push

---
+++ Program_to_reverse_string.py
@@ -0,0 +1,6 @@
+def my_function(reverse_string):
+    for x in reverse_string:
+        print(x)
+my_string1 = "1234abcd"
+reversed_string = my_string1[::-1]
+print(reversed_string)
---
+++ function_to_calculate_sum_numbers.py
@@ -0,0 +1,8 @@
+def sum_numbers(*numbers: tuple) -> tuple:
+    """Sum numbers"""
+    return sum(numbers)
+
+print(sum_numbers(1, 2, 3, 4, 5))
+
+
+
